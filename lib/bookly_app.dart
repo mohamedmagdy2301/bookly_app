f482472418f9f8526485204994eb2652be49f37c
Mohamed magdy <mohammedmego15@gmail.com>
2024-08-15 17:48:19 +0300

    create bookly app

---
+++ lib/bookly_app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class BooklyApp extends StatelessWidget {
+  const BooklyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+    );
+  }
+}
