e1ad06d590175ca823fe7fca7d8aa4b0065d01c9
Mohamed magdy <mohammedmego15@gmail.com>
2024-08-18 21:20:01 +0300

    create search feature

---
+++ lib/features/search/ui/screens/search_screens.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SearchScreen extends StatelessWidget {
+  const SearchScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: Colors.red,
+        child: const Text('Search Screen'),
+      ),
+    );
+  }
+}
