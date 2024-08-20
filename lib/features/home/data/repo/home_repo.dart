0db306a4482f40ab99a3eab1d75a0db5110197e3
Mohamed magdy <mohammedmego15@gmail.com>
2024-08-21 01:56:29 +0300

    create and impl fetchSimilarBooks repo

--- lib/features/home/data/repo/home_repo.dart
+++ lib/features/home/data/repo/home_repo.dart
@@ -1,9 +1,11 @@
 import 'package:bookly_app/core/errors/failure.dart';
 import 'package:bookly_app/features/home/data/model/books_model.dart';
 import 'package:dartz/dartz.dart';
+import 'package:flutter/foundation.dart';
 
 abstract class HomeRepo {
   Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks();
 
   Future<Either<Failure, List<BookModel>>> fetchNewestBooks();
+  Future<Either<Failure, List<BookModel>>> fetchSimilarBooks(category);
 }
--- lib/features/home/data/repo/home_repo_impl.dart
+++ lib/features/home/data/repo/home_repo_impl.dart
@@ -45,4 +45,23 @@ class HomeRepoImpl implements HomeRepo {
       }
     }
   }
+
+  @override
+  Future<Either<Failure, List<BookModel>>> fetchSimilarBooks(category) async {
+    try {
+      var data = await apiService
+          .get("volumes?Filtering=free-ebooks&Sorting=relevance&q=programming");
+      List<BookModel> booksList = [];
+      for (var items in data["items"]) {
+        booksList.add(BookModel.fromJson(items));
+      }
+      return right(booksList);
+    } catch (e) {
+      if (e is DioException) {
+        return left(ServerFailure.fromDioException(e));
+      } else {
+        return left(ServerFailure(e.toString()));
+      }
+    }
+  }
 }
